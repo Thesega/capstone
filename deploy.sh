198d1a299a08213b8208dc7b6df8928f8a66a062
THESEGA P S <thesegaps.22cse@kongu.edu>
2025-03-26 14:21:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u thesega003 -p dckr _pat_IeeSQE_yehokfyTgQf4ENwIUJfE
+    docker login -u thesega003 -p dckr_pat_SUnhuDtbWEih5ASCqNoGZr2_oqA
     docker tag test thesega003/dev
     docker push thesega003/dev
     docker-compose up -d
