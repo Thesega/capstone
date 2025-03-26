ba80960ea3d258a6348f72628b6bd02eb32d8882
THESEGA P S <thesegaps.22cse@kongu.edu>
2025-03-26 14:41:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u thesega003 -p dckr_pat_SUnhuDtbWEih5ASCqNoGZr2_oqA
-    docker tag test thesega003/dev
-    docker push thesega003/dev
+    docker tag test thesega003/task_jenkins:latest
+    docker push thesega003/task_jenkins:latest
     docker-compose up -d
       
